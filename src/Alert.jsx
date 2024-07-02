23af557c0866ba4ffb1feb9f7b2dbc782a0234ff
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-07-02 20:45:28 +0530

    Revert "file changes from local to main"
    This reverts commit 185e37c1de65074b167ce377bcee3325184d21a7.

--- src/Alert.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Alert({msg,setMsg}) {
-    return (
-        <div style={{width:"120px",padding:"0.35rem"}} className="alert alert-success" role="alert">
-            success: {msg}
-        </div>
-    )
-}
